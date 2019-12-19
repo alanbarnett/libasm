1bbba73859bac2853eda88109836bdee30b4fb63
Alan Barnett <alanbarnett328@gmail.com>
2019-12-19 15:46:29 -0800

    feat(test): compile-able assembly function
    Adds a main.c which uses an undefined add function, and changes the
    add.s from the old add.c file that GCC turned into assembly into the add
    function that this main uses. Also includes a Makefile to compile the
    two objects and link them into an executable.

--- add/add.s
+++ add/add.s
@@ -1,51 +1,8 @@
-	.file	"add.c"
-	.text
-	.globl	add
-	.type	add, @function
+global add
+
+section .text:
+
 add:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	%edi, -4(%rbp)
-	movl	%esi, -8(%rbp)
-	movl	-4(%rbp), %edx
-	movl	-8(%rbp), %eax
-	addl	%edx, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
+	add rdi, rsi		; add parameters
+	mov rax, rdi		; exit with added value
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	add, .-add
-	.globl	main
-	.type	main, @function
-main:
-.LFB1:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	$15, -12(%rbp)
-	movl	$27, -8(%rbp)
-	movl	-8(%rbp), %edx
-	movl	-12(%rbp), %eax
-	movl	%edx, %esi
-	movl	%eax, %edi
-	call	add
-	movl	%eax, -4(%rbp)
-	movl	-4(%rbp), %eax
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE1:
-	.size	main, .-main
-	.ident	"GCC: (GNU) 9.2.0"
-	.section	.note.GNU-stack,"",@progbits
---
+++ add/main.c
@@ -0,0 +1,10 @@
+int	add(int a, int b);
+
+int	main(int argc, char **argv)
+{
+	int	x;
+
+	(void)argv;
+	x = add(argc, 10);
+	return (x);
+}

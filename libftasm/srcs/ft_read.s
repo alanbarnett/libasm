3f3413a2184c52087a670d30305de04e6778e33c
Alan Barnett <alanbarnett328@gmail.com>
2020-05-04 06:36:47 -0700

    feat(read+write): set errno

--- libftasm/srcs/ft_read.s
+++ libftasm/srcs/ft_read.s
@@ -1,8 +1,23 @@
 global ft_read
 
+extern ft_set_errno
+
 section .text:
 
 ft_read:
+
+	push rbp
+	mov rbp, rsp
+
 	mov rax, 0x00	; read syscall
 	syscall			; call read, using parameters already loaded
+
+	cmp rax, 0x00
+	jge end			; if positive return, go to end
+
+	call ft_set_errno ; else set the errno
+
+end:
+	pop rbp
+
 	ret
--- libftasm/srcs/ft_write.s
+++ libftasm/srcs/ft_write.s
@@ -1,8 +1,23 @@
 global ft_write
 
+extern ft_set_errno
+
 section .text:
 
 ft_write:
+
+	push rbp
+	mov rbp, rsp
+
 	mov rax, 0x01	; write syscall
 	syscall			; call write, using parameters already loaded
+
+	cmp rax, 0x00
+	jge end			; if positive return, go to end
+
+	call ft_set_errno ; else set the errno
+
+end:
+	pop rbp
+
 	ret
